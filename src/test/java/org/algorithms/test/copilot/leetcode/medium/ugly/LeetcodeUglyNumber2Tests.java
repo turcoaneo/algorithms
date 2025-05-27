709f933d80b9dd063ca46e7c575c5a29d3fd5de9
Ovidiu Turcoane <ovidiu.turcoane@gmail.com>
2025-05-27 15:45:12 +0300

    Ugly Number II.

--- src/test/java/org/algorithms/test/copilot/leetcode/medium/ugly/LeetcodeUglyNumber2Tests.java
+++ src/test/java/org/algorithms/test/copilot/leetcode/medium/ugly/LeetcodeUglyNumber2Tests.java
@@ -1,4 +1,4 @@
-package org.algorithms.test.copilot.leetcode.hard.median;
+package org.algorithms.test.copilot.leetcode.medium.ugly;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.params.ParameterizedTest;
@@ -11,21 +11,21 @@ import java.util.stream.Stream;
 
 
 @SpringBootTest
-public class LeetcodeMedianTests {
+public class LeetcodeUglyNumber2Tests {
     @Autowired
-    org.algorithms.test.copilot.leet.hard.median.Solution solution;
+    org.algorithms.test.copilot.leet.hard.ugly.Solution solution;
 
-    static Stream<Arguments> getMedianArgs() {
+    static Stream<Arguments> getArgs() {
         return Stream.of(
-                Arguments.of(new int[]{1, 3}, new int[]{2}, 2.0D),
-                Arguments.of(new int[]{1, 2}, new int[]{3, 4}, 2.5D)
+                Arguments.of(10, 12),
+                Arguments.of(1, 1)
         );
     }
 
     @ParameterizedTest
-    @MethodSource("getMedianArgs")
-    void test(int[] nums1, int[] nums2, double expected) {
-        double actual = solution.findMedianSortedArrays(nums1, nums2);
+    @MethodSource("getArgs")
+    void test(int n, int expected) {
+        int actual = solution.nthUglyNumber(n);
         System.out.println(actual);
         Assertions.assertEquals(expected, actual);
     }
